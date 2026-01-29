bb22c2fc4e0cb8fdd5623186778701f5499d42e9
Joshua Restivo <jrestivo@xyence.io>
2026-01-29 13:27:08 +0000

    Fix RQ worker connection setup

--- backend/articles/worker.py
+++ backend/articles/worker.py
@@ -1,15 +1,14 @@
 import os
 
 import redis
-from rq import Connection, Worker
+from rq import Worker
 
 
 def main() -> None:
     redis_url = os.environ.get("XYENCE_JOBS_REDIS_URL", "redis://redis:6379/0")
     conn = redis.Redis.from_url(redis_url)
-    with Connection(conn):
-        worker = Worker(["default"])
-        worker.work()
+    worker = Worker(["default"], connection=conn)
+    worker.work()
 
 
 if __name__ == "__main__":
